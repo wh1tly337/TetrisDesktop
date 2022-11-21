c3280fbf6edd36d152b10c820438a61c68ff5784
wh1tly337 <wh1tly337@gmail.com>
2022-11-21 22:08:29 +0500

    background music added + names update

--- MyTetris/main_code/MainMenu.java
+++ MyTetris/main_code/MainMenu.java
@@ -12,7 +12,7 @@ public class MainMenu extends JFrame {
         JPanel panel = new JPanel();
         add(panel);
 
-        ImageIcon logo = new ImageIcon("/Users/user/IdeaProjects/TetrisMacOS/MyTetris/TETRIS.png");
+        ImageIcon logo = new ImageIcon("/Users/user/IdeaProjects/TetrisMacOS/MyTetris/TETRIS_logo.png");
         JLabel background = new JLabel("", logo, JLabel.CENTER);
         background.setBounds(0, 40, 550, 120);
         add(background);
